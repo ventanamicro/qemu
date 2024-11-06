c991dcd5bfe1456c56ad1b0a023c330cc57db081
Rahul Pathak <rpathak@ventanamicro.com>
2024-11-06 09:23:39 +0530

    hw/misc: Update system reset type names in platform code
    Change the system reset types names as per the
    specification matching the SBI reset types.
    
    Signed-off-by: Rahul Pathak <rpathak@ventanamicro.com>

--- hw/misc/riscv_rpmi_sysreset.c
+++ hw/misc/riscv_rpmi_sysreset.c
@@ -7,25 +7,17 @@
 void add_sysreset_group(struct rpmi_context *rctx);
 void rpmi_do_system_reset(void *priv, rpmi_uint32_t sysreset_type);
 
-/** RPMI System Reset types */
-enum rpmi_sysrst_reset_type {
-    RPMI_SYSRST_SHUTDOWN = 0,
-    RPMI_SYSRST_COLD_RESET = 1,
-    RPMI_SYSRST_WARM_RESET = 2,
-    RPMI_SYSRST_MAX_IDN_COUNT,
-};
-
-const rpmi_uint32_t rpmi_reset_types[2] = {RPMI_SYSRST_SHUTDOWN,
-    RPMI_SYSRST_COLD_RESET};
+const rpmi_uint32_t rpmi_reset_types[2] = {RPMI_SYSRST_TYPE_SHUTDOWN,
+    RPMI_SYSRST_TYPE_COLD_REBOOT};
 
 void rpmi_do_system_reset(void *priv, rpmi_uint32_t reset_type)
 {
-    if (reset_type == RPMI_SYSRST_WARM_RESET ||
-        reset_type == RPMI_SYSRST_COLD_RESET) {
+    if (reset_type == RPMI_SYSRST_TYPE_WARM_REBOOT ||
+        reset_type == RPMI_SYSRST_TYPE_COLD_REBOOT) {
         qemu_log_mask(LOG_GUEST_ERROR,
                       "%s: rebooting..\n",  __func__);
         qemu_system_reset_request(SHUTDOWN_CAUSE_GUEST_RESET);
-    } else if (reset_type == RPMI_SYSRST_SHUTDOWN) {
+    } else if (reset_type == RPMI_SYSRST_TYPE_SHUTDOWN) {
         qemu_log_mask(LOG_GUEST_ERROR,
                       "%s: Shutting down..\n", __func__);
         exit(0);
